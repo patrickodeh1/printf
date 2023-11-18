38f244a01f776f407a528b649af168f010c873fc
Patrick Odeh <vyltyan@gmail.com>
2023-11-17 23:59:33 -0800

    print_oct.c

---
+++ print_oct.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_octal - converts to octal
+ * @val: variable parameter of type va_list
+ * Return: Number to be printed.
+ */
+int print_octal(va_list val)
+{
+	int i, count = 0;
+	int *array;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int tmp = num;
+
+	while (num / 8 != 0)
+	{
+		num = num / 8;
+		count++;
+	}
+	count++;
+	array = malloc(sizeof(int) * count);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < count; i++)
+	{
+		array[i] = tmp % 8;
+		tmp = tmp / 8;
+	}
+	for (i = count - 1; i >= 0; i--)
+	{
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (count);
+
+}
