bcd8180285d7eee00667c2cb52c324ff70460ffb
Clinton Nwokocha <leoclinton2011@hotmail.com>
2023-11-17 20:17:53 -0800

    solve Task 2

---
+++ con_bin.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * con_bin - converts to binary
+ * @val: argument
+ * Return: integer
+ */
+int con_bin(va_list val)
+{
+	int flag = 0;
+	int count = 0;
+	int i, a = 1, b;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int p;
+
+	for (i = 0; i < 32; i++)
+	{
+		p = ((a << (32 - i)) & num);
+		if (p >> (31 - i))
+			flag = 1;
+		if (flag)
+		{
+			b = p >> (31 - i);
+			_putchar(b + 48);
+			count++;
+		}
+	}
+	if (count == 0)
+	{
+		count++;
+		_putchar('0');
+	}
+	return (count);
+}
+
