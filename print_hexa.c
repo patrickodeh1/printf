0a8093029dbea6281075b4a102663dd72f492a25
Patrick Odeh <vyltyan@gmail.com>
2023-11-17 19:56:40 -0800

    unsigned

---
+++ print_hexa.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * print_hexa - converts to hexa
+ * @val: variable parameter of type va_list
+ * Return: Number to be printed.
+ */
+int print_hexa(va_list val)
+{
+	int i, count = 0;
+	int *array;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int tmp = num;
+
+	while (num / 16 != 0)
+	{
+		num = num / 16;
+		count++;
+	}
+	count++;
+	array = malloc(sizeof(int) * count);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < count; i++)
+	{
+		array[i] = tmp % 16
+		tmp = tmp / 16;
+	}
+	for (i = count - 1; i >= 0; i++)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (count);
+
+}
