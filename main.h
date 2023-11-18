4f592604f1d42a1aeca21c4de31f79f996022052
Clinton Nwokocha <leoclinton2011@hotmail.com>
2023-11-17 19:08:56 -0800

    update main.h

--- main.h
+++ main.h
@@ -21,4 +21,17 @@ int printf_string(va_list val);
 int _strlen(char *str);
 int _strlenc(const char *str);
 int print_37(void);
+int print_dec(va_list args);
+int print_int(va_list args);
+int con_bin(va_list val);
+int print_unsigned(va_list args);
+int print_octal(va_list val);
+int print_hexa(va_list val);
+int print_cap_hexa(va_list val);
+int print_extra_hex(unsigned int num);
+int print_spec_str(va_list val);
+int print_extra_small_hex(unsigned long int num);
+int print_pointer(va_list val);
+int print_reverse(va_list val);
+int print_rot13(va_list val);
 #endif
