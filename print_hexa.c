130beb8a2b0945a788eb9bfcb907caa7d46f3ddd
Patrick Odeh <vyltyan@gmail.com>
2023-11-17 20:00:06 -0800

    hexa

--- print_hexa.c
+++ print_hexa.c
@@ -18,10 +18,6 @@ int print_hexa(va_list val)
 	}
 	count++;
 	array = malloc(sizeof(int) * count);
-	if (array == NULL)
-	{
-		return (NULL);
-	}
 	for (i = 0; i < count; i++)
 	{
 		array[i] = tmp % 16
